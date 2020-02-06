c4c0e64d53768a490c54c93df98660c6c4512a45
njh7799 <rainy_waltz@naver.com>
2020-02-06 15:32:00 +0900

    [Feat] js 적용

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default props => {
-  return <Title>Hi {props.name}!</Title>;
+export default ({ name }) => {
+  function nameClickHandler() {
+    alert(name);
+  }
+  return <Title onClick={nameClickHandler}>Hi {name}!</Title>;
 };
 
 const Title = styled.h1`
